41affcd5598c6d76c77a73814d905bff878c3ce9
Kyle Miller <millro04@luther.edu>
2016-05-03 11:58:27 -0500

    Added bootstrap template

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
+from flask import Flask, jsonify, render_template
+from flask import request
+
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run()
 
